ca33d53ec83f48086a010342765029da34d12dfa
Dinithi Epitakaduwa <dinithiepi@gmail.com>
2025-12-08 09:38:15 +0530

    Revert "depo14"
    This reverts commit e348b12610ebb6efbaeaffddeb726db65d2a940e.

--- backend/disaster-aid-backend/gateway/src/index.js
+++ backend/disaster-aid-backend/gateway/src/index.js
@@ -55,6 +55,6 @@ app.get('/', (req, res) => {
 });
 
 // START
-app.listen(process.env.PORT || 4001, () => {
-    console.log(`Gateway running on port ${process.env.PORT || 4001}...`);
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Gateway running...');
 });
